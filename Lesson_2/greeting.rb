f311ba835c005b2480706bdafee16f6f084df0c5
Varvara Korableva <v.korableva1990@yandex.ru>
2023-07-20 12:26:52 +0300

    changed greeting fnc- added small valid

--- Lesson_2/greeting.rb
+++ Lesson_2/greeting.rb
@@ -8,6 +8,11 @@ def greeting
     puts "Введи свой возраст:"
     age = gets.chomp.to_i
 
+    while age <= 0
+        puts "С возрастом что-то не так, попробуй снова:"
+        age = gets.chomp.to_i
+    end
+
     if age < 18
         puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано." 
     else     
